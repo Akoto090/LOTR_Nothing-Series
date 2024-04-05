e35c807bd74df55280d6c2e077e87c7606efe52c
xImBlank <119544331+xImBlank@users.noreply.github.com>
2024-04-05 17:02:47 +0200

    Create EventHandler.java

---
+++ src/main/java/net/akoto090/lotr/event/EventHandler.java
@@ -0,0 +1,38 @@
+package net.akoto090.lotr.event;
+
+import net.akoto090.lotr.LotrMod;
+import net.akoto090.lotr.block.ModBlocks;
+import net.akoto090.lotr.entity.custom.PlateProjectileEntity;
+import net.minecraft.stats.Stats;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = LotrMod.MOD_ID)
+public class EventHandler {
+    @SubscribeEvent
+    public static void onRightclickItem(PlayerInteractEvent.RightClickItem event){
+        if(!event.getEntity().level().isClientSide) {
+            if ((event.getEntity() instanceof Player)
+                    && (event.getEntity().getItemInHand(event.getEntity().getUsedItemHand()).getItem()
+                    == ModBlocks.PLATE_BLOCK.get().asItem())) {
+
+                Player p = event.getEntity();
+                Level l = event.getLevel();
+                PlateProjectileEntity PlateProjectile = new PlateProjectileEntity(l,p);
+                ItemStack item = p.getItemInHand(p.getUsedItemHand());
+                PlateProjectile.setItem(item);
+                PlateProjectile.shootFromRotation(p, p.getXRot(), p.getYRot(), 0.0F, 1.5F, 0.0F);
+                event.getLevel().addFreshEntity(PlateProjectile);
+
+                p.awardStat(Stats.ITEM_USED.get(p.getUseItem().getItem()));
+                if (!p.getAbilities().instabuild) {
+                    item.shrink(1);
+                }
+            }
+        }
+    }
+}
